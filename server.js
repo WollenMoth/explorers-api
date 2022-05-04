89eb6cbdc617da2ea4c3bba6fa166d5a324ff8f2
André Díaz <andre290800@gmail.com>
2022-05-03 20:19:37 -0500

    GET /explorers endpoint

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 
 const app = express();
 const port = 3000;
 
+const prisma = new PrismaClient();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "alive" });
+});
+
+app.get("/explorers", async (req, res) => {
+    const allExplorers = await prisma.explorer.findMany({});
+    res.json(allExplorers);
 });
 
 app.listen(port, () => {
