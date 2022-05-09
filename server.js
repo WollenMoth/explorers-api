610dfa7ea7cc7fce47f3ed938e1281d60cee4cb3
André Díaz <andre290800@gmail.com>
2022-05-09 11:34:16 -0500

    add trycatch blocks

--- server.js
+++ server.js
@@ -30,28 +30,49 @@ app.get("/explorers/:id", async (req, res) => {
 });
 
 app.post("/explorers", async (req, res) => {
-    const explorer = { ...req.body };
-    const message = "Explorer created";
-    await prisma.explorer.create({ data: explorer });
-    res.status(201).json({ message });
+    try {
+        const explorer = { ...req.body };
+        const message = "Explorer created";
+        await prisma.explorer.create({ data: explorer });
+        res.status(201).json({ message });
+    } catch (error) {
+        if (error.code === "P2002")
+            res.status(400).json({
+                detail: "Nombre o username ya está en uso, elije otro",
+            });
+    }
 });
 
 app.put("/explorers/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const explorer = { ...req.body };
-    const message = "Explorer updated";
-    await prisma.explorer.update({
-        where: { id: id },
-        data: explorer,
-    });
-    res.json({ message });
+    try {
+        const id = parseInt(req.params.id);
+        const explorer = { ...req.body };
+        const message = "Explorer updated";
+        await prisma.explorer.update({
+            where: { id: id },
+            data: explorer,
+        });
+        res.json({ message });
+    } catch (error) {
+        if (error.code === "P2002")
+            res.status(400).json({
+                detail: "Nombre o username ya está en uso, elije otro",
+            });
+    }
 });
 
 app.delete("/explorers/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const message = "Explorer deleted";
-    await prisma.explorer.delete({ where: { id: id } });
-    res.json({ message });
+    try {
+        const id = parseInt(req.params.id);
+        const message = "Explorer deleted";
+        await prisma.explorer.delete({ where: { id: id } });
+        res.json({ message });
+    } catch (error) {
+        if (error.code === "P2025")
+            res.status(400).json({
+                detail: "Explorer no existente",
+            });
+    }
 });
 
 app.get("/students", async (req, res) => {
@@ -66,28 +87,49 @@ app.get("/students/:id", async (req, res) => {
 });
 
 app.post("/students", async (req, res) => {
-    const student = { ...req.body };
-    const message = "Student created";
-    await prisma.student.create({ data: student });
-    res.status(201).json({ message });
+    try {
+        const student = { ...req.body };
+        const message = "Student created";
+        await prisma.student.create({ data: student });
+        res.status(201).json({ message });
+    } catch (error) {
+        if (error.code === "P2002")
+            res.status(400).json({
+                detail: "Nombre ya está en uso, elije otro",
+            });
+    }
 });
 
 app.put("/students/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const student = { ...req.body };
-    const message = "Student updated";
-    await prisma.student.update({
-        where: { id: id },
-        data: student,
-    });
-    res.json({ message });
+    try {
+        const id = parseInt(req.params.id);
+        const student = { ...req.body };
+        const message = "Student updated";
+        await prisma.student.update({
+            where: { id: id },
+            data: student,
+        });
+        res.json({ message });
+    } catch (error) {
+        if (error.code === "P2002")
+            res.status(400).json({
+                detail: "Nombre ya está en uso, elije otro",
+            });
+    }
 });
 
 app.delete("/students/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const message = "Student deleted";
-    await prisma.student.delete({ where: { id: id } });
-    res.json({ message });
+    try {
+        const id = parseInt(req.params.id);
+        const message = "Student deleted";
+        await prisma.student.delete({ where: { id: id } });
+        res.json({ message });
+    } catch (error) {
+        if (error.code === "P2025")
+            res.status(400).json({
+                detail: "Estudiante no existente",
+            });
+    }
 });
 
 app.listen(port, () => {
